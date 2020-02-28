4fadd62c273171657871dcc4c69f5214be1b893f
scottbing <bingscott@gmail.com>
2020-02-27 20:09:54 -0700

    Add files via upload

---
+++ SongListGenerator.h
@@ -0,0 +1,19 @@
+#ifndef SONGLISTGENERATOR
+#define SONGLISTGENERATOR
+
+#include <string>
+#include <vector>
+
+class SongListGenerator
+{
+
+
+
+ static void main(std::vector<std::wstring> &args);
+
+
+
+
+
+
+#endif	//#ifndef SONGLISTGENERATOR
